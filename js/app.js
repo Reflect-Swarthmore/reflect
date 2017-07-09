562cc179e73033f4a9aaee91202354566e0551de
Mikelon15 <miguelgutz95@gmail.com>
2017-07-08 21:11:57 -0700

    npm require not working because it is not supported by browser :(

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-var $ = require('jquery');
-window.$ = $;
-require('bootstrap');
+var req = require('quill');
+
+var title = new Quill('#title', {
+  placeholder: 'Titulo...',
+  theme: 'bubble'
+});
+var quill = new Quill('#inputText', {
+  modules: {
+    toolbar: [
+      [{ 'font': [] }],
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'align': [] }],
+      ['bold', 'italic', 'underline'],
+      ['image']
+    ]
+  },
+  placeholder: 'Tell us your life story...',
+  theme: 'snow'  // or 'bubble'
+});
